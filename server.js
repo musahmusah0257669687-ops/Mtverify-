5e3c07bfdcea515c0100bef3c2d2d98c1b08de00
musahmusah0257669687-ops <musahmusah0257669687@gmail.com>
2026-09-08 02:39:15 +0000

    Fix server.js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api/status", (req, res) => {
+  res.json({
+    site: "MtVerify",
+    status: "online"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`MtVerify running on port ${PORT}`);
 });
